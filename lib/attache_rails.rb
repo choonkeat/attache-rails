83f897079fe12cae6e11254399e4c444f382aab3
choonkeat <choonkeat@gmail.com>
2015-04-02 03:55:26 +0800

    Lengthen default form expiry

--- lib/attache_rails.rb
+++ lib/attache_rails.rb
@@ -3,7 +3,7 @@ module AttacheRails
   ATTACHE_UPLOAD_URL      = ENV.fetch('ATTACHE_UPLOAD_URL')      { "#{ATTACHE_URL}/upload" }
   ATTACHE_DOWNLOAD_URL    = ENV.fetch('ATTACHE_DOWNLOAD_URL')    { "#{ATTACHE_URL}/view" }
   ATTACHE_DELETE_URL      = ENV.fetch('ATTACHE_DELETE_URL')      { "#{ATTACHE_URL}/delete" }
-  ATTACHE_UPLOAD_DURATION = ENV.fetch('ATTACHE_UPLOAD_DURATION') { "600" }.to_i # signed upload form expires in 10 minutes
+  ATTACHE_UPLOAD_DURATION = ENV.fetch('ATTACHE_UPLOAD_DURATION') { 3.hours }.to_i # expires signed upload form
 end
 
 require "attache_rails/engine"
