16bea41ce1c02ad7266a6261f8e85e1a92d8fe83
choonkeat <choonkeat@gmail.com>
2015-10-13 12:26:43 +0800

    Update to add "class" and "multiple" html attributes externally; not part of "attache_field_options"

--- lib/attache_rails/model.rb
+++ lib/attache_rails/model.rb
@@ -31,7 +31,7 @@ module AttacheRails
 
       def has_one_attache(name)
         attache_setup_column(name)
-        define_method "#{name}_options",    -> (geometry, options = {}) { attache_field_options(self.send(name), geometry, Hash(multiple: false).merge(options)) }
+        define_method "#{name}_options",    -> (geometry, options = {}) { Hash(class: 'enable-attache', multiple: false).merge(attache_field_options(self.send(name), geometry, options)) }
         define_method "#{name}_url",        -> (geometry)               { attache_field_urls(self.send(name), geometry).try(:first) }
         define_method "#{name}_attributes", -> (geometry)               { attache_field_attributes(self.send(name), geometry).try(:first) }
         define_method "#{name}=",           -> (value)                  { super(attache_field_set(Array.wrap(value)).try(:first)) }
@@ -41,7 +41,7 @@ module AttacheRails
 
       def has_many_attaches(name)
         attache_setup_column(name)
-        define_method "#{name}_options",    -> (geometry, options = {}) { attache_field_options(self.send(name), geometry, Hash(multiple: true).merge(options)) }
+        define_method "#{name}_options",    -> (geometry, options = {}) { Hash(class: 'enable-attache', multiple: true).merge(attache_field_options(self.send(name), geometry, options)) }
         define_method "#{name}_urls",       -> (geometry)               { attache_field_urls(self.send(name), geometry) }
         define_method "#{name}_attributes", -> (geometry)               { attache_field_attributes(self.send(name), geometry) }
         define_method "#{name}=",           -> (value)                  { super(attache_field_set(Array.wrap(value))) }
