c11e948db868f11bf5a2e00a1c8e447511fa5ff4
John Wilkinson <jcwilk@gmail.com>
2016-06-09 04:17:14 +0800

    Force railtie to load
    This is a rails gem so it's reasonable to expect that rails will be available to `require`

--- lib/attache/rails.rb
+++ lib/attache/rails.rb
@@ -5,5 +5,5 @@ end
 
 require "attache/rails/engine"
 require "attache/rails/model"
-require 'attache/rails/railtie' if defined? ::Rails::Railtie
+require 'attache/rails/railtie'
 require "attache/api/test" if defined?(Rails) && Rails.env.test?
